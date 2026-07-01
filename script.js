16d2c351b681d3adf87ab57e775496c95d4c09b9
Almareem15-del <tripathimaanas59@gmail.com>
2026-07-02 01:55:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.querySelector("button").addEventListener("click", function () {
-    alert("Welcome to GCC Vision Hub! More features are coming soon.");
+
+window.location.href="ideas.html";
+
 });
